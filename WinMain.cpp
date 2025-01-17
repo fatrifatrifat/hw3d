8099b4f6e1ae791553705f54a0c9d30a803e6385
fatrifatrifat <rifatkatranci19@gmail.com>
2025-01-16 22:30:19 -0500

    Back to the basics. Finally got something showing on the screen... had so many problems. Time to really start doing things :)

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,75 +1,277 @@
-//***************************************************************************************
-// Init Direct3D.cpp by Frank Luna (C) 2011 All Rights Reserved.
-//
-// Demonstrates the sample framework by initializing Direct3D, clearing 
-// the screen, and displaying frame stats.
-//
-//***************************************************************************************
+#include <Windows.h>
+#include <d3d11.h>
+#include <DirectXMath.h>
+#include <d3dcompiler.h>
+#include <dxgi.h>
+#include <algorithm>
 
-#include "d3dApp.h"
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "D3DCompiler.lib")
 
-class InitDirect3DApp : public D3DApp
+using namespace DirectX;
+
+namespace Colors
 {
-public:
-	InitDirect3DApp(HINSTANCE hInstance);
-	~InitDirect3DApp();
+	XMFLOAT4 red = { 1.0f, 0.0f, 0.0f, 1.0f };
+	XMFLOAT4 green = { 0.0f, 1.0f, 0.0f, 1.0f };
+	XMFLOAT4 blue = { 0.0f, 0.0f, 1.0f, 1.0f };
+}
 
-	bool Init();
-	void OnResize();
-	void UpdateScene(float dt);
-	void DrawScene();
-};
+HWND hMainWnd;
+
+ID3D11Device* pDevice;
+ID3D11DeviceContext* pImmediateContext;
+IDXGISwapChain* pSwapChain;
+ID3D11RenderTargetView* pRVT;
+
+bool InitWin(HINSTANCE hInstance);
+bool InitD3D();
+LRESULT CALLBACK MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+int Run();
+
+void ClearBuffer();
+void DoFrame();
+void EndFrame();
+
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
 	PSTR cmdLine, int showCmd)
 {
-	// Enable run-time memory check for debug builds.
-	#if defined(DEBUG) | defined(_DEBUG)
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-	#endif
+	if (!InitWin(hInstance))
+	{
+		MessageBox(0, L"Init Window Failed", 0, 0);
+		return -1;
+	}
 
-	InitDirect3DApp theApp(hInstance);
+	if (!InitD3D())
+	{
+		MessageBox(0, L"Init D3D Failed", 0, 0);
+		return -1;
+	}
 
-	if (!theApp.Init())
-		return 0;
-
-	return theApp.Run();
+	Run();
 }
 
-InitDirect3DApp::InitDirect3DApp(HINSTANCE hInstance)
-	: D3DApp(hInstance)
+bool InitD3D()
 {
-}
+	D3D_FEATURE_LEVEL featureLevel;
 
-InitDirect3DApp::~InitDirect3DApp()
-{
-}
+	DXGI_SWAP_CHAIN_DESC sd;
+	sd.BufferDesc.Width = 800;
+	sd.BufferDesc.Height = 600;
+	sd.BufferDesc.RefreshRate.Numerator = 60;
+	sd.BufferDesc.RefreshRate.Denominator = 1;
+	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	sd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	sd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
 
-bool InitDirect3DApp::Init()
-{
-	if (!D3DApp::Init())
+	sd.SampleDesc.Count = 1;
+	sd.SampleDesc.Quality = 0;
+
+	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	sd.BufferCount = 1;
+	sd.OutputWindow = hMainWnd;
+	sd.Windowed = true;
+	sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	sd.Flags = 0;
+
+	HRESULT hr = D3D11CreateDeviceAndSwapChain(0, D3D_DRIVER_TYPE_HARDWARE, 0, 0, 0, 0, D3D11_SDK_VERSION, &sd, &pSwapChain, &pDevice, &featureLevel, &pImmediateContext);
+
+	if (FAILED(hr))
+	{
+		MessageBox(0, L"CreateDeviceAndSwapChain Failed", 0, 0);
 		return false;
+	}
+
+	ID3D11Texture2D* pBackBuffer;
+	pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), reinterpret_cast<void**>(&pBackBuffer));
+	pDevice->CreateRenderTargetView(pBackBuffer, 0, &pRVT);
+	pBackBuffer->Release();
+
+	pImmediateContext->ClearRenderTargetView(pRVT, reinterpret_cast<const float*>(&Colors::blue));
+
+	pSwapChain->Present(0, 0);
 
 	return true;
 }
 
-void InitDirect3DApp::OnResize()
+bool InitWin(HINSTANCE hInstance)
 {
-	D3DApp::OnResize();
+	WNDCLASS wc;
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = MsgProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(0, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(0, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)GetStockObject(NULL_BRUSH);
+	wc.lpszMenuName = 0;
+	wc.lpszClassName = L"D3DWndClassName";
+
+	if (!RegisterClass(&wc))
+	{
+		MessageBox(0, L"RegisterClass Failed.", 0, 0);
+		return false;
+	}
+
+	// Compute window rectangle dimensions based on requested client area dimensions.
+	RECT R = { 0, 0, 800, 400 };
+	AdjustWindowRect(&R, WS_OVERLAPPEDWINDOW, false);
+	int width = R.right - R.left;
+	int height = R.bottom - R.top;
+
+	hMainWnd = CreateWindow(L"D3DWndClassName", L"Rifat Katranci",
+		WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, width, height, 0, 0, hInstance, 0);
+	if (!hMainWnd)
+	{
+		MessageBox(0, L"CreateWindow Failed.", 0, 0);
+		return false;
+	}
+
+	ShowWindow(hMainWnd, SW_SHOW);
+	UpdateWindow(hMainWnd);
+
+	return true;
 }
 
-void InitDirect3DApp::UpdateScene(float dt)
+int Run()
 {
+	MSG msg = {};
 
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+			//ClearBuffer();
+			DoFrame();
+			EndFrame();
+		}
+	}
+
+	return (int)msg.wParam;
 }
 
-void InitDirect3DApp::DrawScene()
+void ClearBuffer()
 {
-	assert(md3dImmediateContext);
-	assert(mSwapChain);
+	pImmediateContext->ClearRenderTargetView(pRVT, reinterpret_cast<const float*>(&Colors::blue));
+}
 
-	md3dImmediateContext->ClearRenderTargetView(mRenderTargetView, reinterpret_cast<const float*>(&Colors::Blue));
-	md3dImmediateContext->ClearDepthStencilView(mDepthStencilView, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+void DoFrame()
+{
+	struct Vertex
+	{
+		float x;
+		float y;
+	};
 
-	mSwapChain->Present(0, 0);
+	// create vertex buffer (1 2d triangle at center of screen)
+	const Vertex vertices[] =
+	{
+		{ 0.0f,0.5f },
+		{ 0.5f,-0.5f },
+		{ -0.5f,-0.5f },
+	};
+	ID3D11Buffer* pVertexBuffer;
+	D3D11_BUFFER_DESC bd = {};
+	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bd.Usage = D3D11_USAGE_DEFAULT;
+	bd.CPUAccessFlags = 0u;
+	bd.MiscFlags = 0u;
+	bd.ByteWidth = sizeof(vertices);
+	bd.StructureByteStride = sizeof(Vertex);
+	D3D11_SUBRESOURCE_DATA sd = {};
+	sd.pSysMem = vertices;
+	pDevice->CreateBuffer(&bd, &sd, &pVertexBuffer);
+
+	// Bind vertex buffer to pipeline
+	const UINT stride = sizeof(Vertex);
+	const UINT offset = 0u;
+	pImmediateContext->IASetVertexBuffers(0u, 1u, &pVertexBuffer, &stride, &offset);
+
+
+	// create pixel shader
+	ID3D11PixelShader* pPixelShader;
+	ID3DBlob* pBlob;
+	D3DReadFileToBlob(L"PixelShader.cso", &pBlob);
+	pDevice->CreatePixelShader(pBlob->GetBufferPointer(), pBlob->GetBufferSize(), nullptr, &pPixelShader);
+
+	// bind pixel shader
+	pImmediateContext->PSSetShader(pPixelShader, nullptr, 0u);
+
+
+	// create vertex shader
+	ID3D11VertexShader* pVertexShader;
+	D3DReadFileToBlob(L"VertexShader.cso", &pBlob);
+	pDevice->CreateVertexShader(pBlob->GetBufferPointer(), pBlob->GetBufferSize(), nullptr, &pVertexShader);
+
+	// bind vertex shader
+	pImmediateContext->VSSetShader(pVertexShader, nullptr, 0u);
+
+
+	// input (vertex) layout (2d position only)
+	ID3D11InputLayout* pInputLayout;
+	const D3D11_INPUT_ELEMENT_DESC ied[] =
+	{
+		{ "POSITION",0,DXGI_FORMAT_R32G32_FLOAT,0,0,D3D11_INPUT_PER_VERTEX_DATA,0 },
+	};
+	pDevice->CreateInputLayout(
+		ied, (UINT)std::size(ied),
+		pBlob->GetBufferPointer(),
+		pBlob->GetBufferSize(),
+		&pInputLayout
+	);
+
+	// bind vertex layout
+	pImmediateContext->IASetInputLayout(pInputLayout);
+
+
+	// bind render target
+	pImmediateContext->OMSetRenderTargets(1u, &pRVT, nullptr);
+
+
+	// Set primitive topology to triangle list (groups of 3 vertices)
+	pImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+
+	// configure viewport
+	D3D11_VIEWPORT vp;
+	vp.Width = 800;
+	vp.Height = 600;
+	vp.MinDepth = 0;
+	vp.MaxDepth = 1;
+	vp.TopLeftX = 0;
+	vp.TopLeftY = 0;
+	pImmediateContext->RSSetViewports(1u, &vp);
+
+	pImmediateContext->Draw((UINT)std::size(vertices), 0);
+}
+
+void EndFrame()
+{
+	pSwapChain->Present(0, 0);
+}
+
+LRESULT CALLBACK MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_LBUTTONDOWN:
+		MessageBox(0, L"Hi", 0, 0);
+		return 0;
+	case WM_KEYDOWN:
+		if (wParam == VK_ESCAPE)
+			DestroyWindow(hwnd);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hwnd, msg, wParam, lParam);
 }
