ec5984a5321fd3a86aaafb9a2245cdc43a6cf69b
Rifat Katranci <rifatkatrani19@gmail.com>
2025-02-20 22:41:54 -0500

    confusing ass template metaprogramming system for vertex layouts (thanks chili)

--- VertexBuffer.h
+++ VertexBuffer.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Bindable.h"
+#include "Vertex.h"
 
 class VertexBuffer : public Bindable
 {
@@ -18,6 +19,22 @@ public:
 		D3D11_SUBRESOURCE_DATA sd = {};
 		sd.pSysMem = vertices.data();
 		GetDevice(d3dApp)->CreateBuffer(&bd, &sd, &pVertexBuffer);
+	}
+
+	VertexBuffer(D3DApp& gfx, const hw3dexp::VertexBuffer& vbuf)
+		:
+		stride((UINT)vbuf.GetLayout().Size())
+	{
+		D3D11_BUFFER_DESC bd = {};
+		bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+		bd.Usage = D3D11_USAGE_DEFAULT;
+		bd.CPUAccessFlags = 0u;
+		bd.MiscFlags = 0u;
+		bd.ByteWidth = UINT(vbuf.SizeBytes());
+		bd.StructureByteStride = stride;
+		D3D11_SUBRESOURCE_DATA sd = {};
+		sd.pSysMem = vbuf.GetData();
+		GetDevice(gfx)->CreateBuffer(&bd, &sd, &pVertexBuffer);
 	}
 	void Bind(D3DApp& d3dApp) noexcept override;
 
